fbd3b186623cf82b7fd92216b4d4e894655bc9be
Chen2357 <87241041+Chen2357@users.noreply.github.com>
2021-08-03 14:52:23 -0700

    Improve documentation and minor fix
    1. Add documentation to `solve.beta`
    2. Specifies the conditions of `R` in `cholesky`
    3. Fix the issue where `L` might not be lower triangular.

--- QradraticProgramming.R
+++ QradraticProgramming.R
@@ -2,26 +2,40 @@ library(limSolve)
 
 #' Pseudo Cholesky Decomposition
 #' 
-#' Perform Cholesky decomposition with remainder, that is find `L` and `R` such that `x = L %*% t(L) + R`. 
+#' Perform Cholesky decomposition with remainder, that is find `L` and `R` such that `x = L %*% t(L) + R` where there are as many 0 as possible in `R`.
+#' 
+#' @details The algorithm is greedy, meaning the first nonzero component in `R` is as far down as possible in the diagonal.
 #' 
 #' @param x A symmetric matrix
-#' @param tol Tolerance
+#' @param tol Tolerance. Number whose absolute value is less than `tol` is considered 0.
 #' @return A list containing:
 #' `L`: the lower triangular matrix of the Cholseky decomposition
 #' `R`: the remainder matrix 
 cholesky <- function(x, tol = .Machine$double.eps^0.5) {
     L <- matrix(0, nrow = nrow(x), ncol = ncol(x))
     colnames(L) <- colnames(x)
+    j <- 1
     rownames(L) <- rownames(L)
     for (i in seq_len(ncol(L))) {
         if (x[i, i] > tol) {
-            L[, i] <- x[, i] / sqrt(x[i, i])
-            x <- x - L[, i] %*% t(L[, i])
+            L[, j] <- x[, i] / sqrt(x[i, i])
+            x <- x - L[, j] %*% t(L[, j])
+            j <- j + 1
         }
     }
     return(list(L = L, R = x))
 }
 
+#' Solving for beta
+#' 
+#' Find `beta` such that `t(beta) %*% A %*% beta + ||beta||_1` is minimzied subject to `B %*% beta = b`. `||beta||_1` is the 1-norm of `beta`
+#' 
+#' @param A A 6x6 postive-semidefinite matrix
+#' @param B A 6x6 matrix
+#' @param b A vector of 6 numbers
+#' @return A list containing:
+#' `solution`: the desired `beta`, see main description
+#' `value`: the minimum value of `t(beta) %*% A %*% beta + ||beta||_1`
 solve.beta <- function(A, B, b, tol = .Machine$double.eps^0.5) {
     min <- NA
     sol <- NA
