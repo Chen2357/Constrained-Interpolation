56c9db2ba848d7d74bb86b393a18b872475c4e9a
Chen2357 <87241041+Chen2357@users.noreply.github.com>
2021-08-05 14:25:43 -0700

    Add beta method as a slope finding function
    Known issue: the function outputs incorrect values of slope.

--- SlopeFinder.R
+++ SlopeFinder.R
@@ -94,3 +94,47 @@ findSlope.constrainedQuadratic <- function(data) {
     }
     return(slope)
 }
+
+#' Slope Finding using Beta Method (3 points)
+#' 
+#' @param data A `pointData` type that stores the 3 points. It will be converted into `x` and `y`.
+#' @param x A vector of x-coordinates of the 3 points. May be assigned directly.
+#' @param y A vector of y-coordinates of the 3 points. May be assigned directly.
+#' @param betaSolver A function to solve beta in the form of \code{function(A, B, b)} that returns a list containing beta. Can be `solve.beta` (default) or `solve.beta.cholesky`.
+#' @return The slopes at the points of interest.
+findSlope.beta.threePoints <- function(data, x = point.x(data), y = point.y(data), betaSolver = solve.beta) {
+    delta21 <- x[2] - x[1]
+    delta32 <- x[3] - x[2]
+
+    # L_cluster matrix
+    # d21^(-2)  0           -d21^(-2)   d21^(-1)    0           0
+    # 0         0           -d32^(-2)   0           -d32^(-2)   -d32^(-1)
+    # 0         0           0           0           1           0
+    # 0         d21^(-1)    0           -d21^(-1)   0           0
+    # 0         0           0           d32^(-1)    0           -d32^(-1)
+    # 0         0           0           0           0           1
+    L <- matrix(c(
+        delta21^(-2),0,0,0,0,0,
+        0,0,0,delta21^(-1),0,0,
+        -delta21^(-2),-delta32^(-2),0,0,0,0,
+        delta21^(-1),0,0,-delta21^(-1),delta32^(-1),0,
+        0,-delta32^(-2),1,0,0,0,
+        0,-delta32^(-1),0,0,-delta32^(-1),1),
+        nrow = 6, ncol = 6
+    )
+
+    L_inv <- Solve(L)
+
+    # L_phi <- diag(c(0, 1/(2*sqrt(y[1])), 0, 1/(2*sqrt(y[2])), 0, 1/(2*sqrt(y[3]))))
+    # A <- t(L_phi) %*% L_phi
+
+    A <- diag(c(0, 1/(4*y[1]), 0, 1/(4*y[2]), 0, 1/(4*y[3])))
+
+    B <- diag(c(1, 0, 1, 0, 1, 0)) %*% L_inv
+    b <- c(y[1], 0, y[2], 0, y[3], 0)
+
+    beta <- betaSolver(A = A, B = B, b = b)
+    p <- L_inv %*% beta
+    print(p)
+    return(p[c(2,4,6)])
+}
