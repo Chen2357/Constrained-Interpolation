6a32e34724f933276acf4dd1a0c735e121f71a6d
Chen2357 <87241041+Chen2357@users.noreply.github.com>
2021-09-23 00:28:32 -0700

    Use package `rrinterp` instead
    Removed codes that are in the `rrinerp` package

--- QPExample.R
+++
@@ -1,7 +0,0 @@
-library(limSolve)
-source("QuadraticProgramming.R")
-
-A <- diag(c(1 / 2, 1 / 3, 1 / 4, 1, 2, 3))
-B <- diag(c(2, 3, 6, 0, 0, 0))
-b <- c(1, 1, 1, 0, 0, 0)
-result <- solve.beta(A, B, b)
