cefad340c5ada7e9aa66689dca41c59d75b0e9f3
Chen2357 <87241041+Chen2357@users.noreply.github.com>
2022-01-16 18:59:02 -0800

    Add initialization method to `whitneyField`

--- 2D/WhitneyField.R
+++ 2D/WhitneyField.R
@@ -3,6 +3,33 @@ whitneyField <- setClass(
     slots = c(
         x = "numeric",
         y = "numeric",
-        s = "matrix"
+        coef = "matrix"
     )
+)
+
+setValidity("whitneySquare", function(object) {
+    if (length(object@x) != length(object@y)) {
+        return("Numbers of x and y data are mismatched")
+    } else if (ncol(object@field) != 3) {
+        return("Number of columns of coef must be 3")
+    } else if (nrow(object@field) != length(object@x)) {
+        return("Number of x data and number of columns of coef mismatched")
+    }
+    return(TRUE)
+})
+
+setMethod("initialize", "whitneyField",
+    function(.Object, x = numeric(0), y = numeric(0), coef = matrix(nrow=length(x), ncol=3)) {
+        .Object@x <- x
+        .Object@y <- y
+
+        if (!is.matrix(coef)) {
+            coef <- matrix(coef, nrow=length(x), ncol=3)
+        }
+
+        .Object@coef <- coef
+
+        validObject(.Object)
+        return(.Object)
+    }
 )
