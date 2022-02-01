5c0fc7cb379962065685e443fd8a83c3f3d67e5a
Chen2357 <87241041+Chen2357@users.noreply.github.com>
2022-01-31 20:39:42 -0800

    Plotting example
    The patching function does not produce an intended plot. There seem to be some unusual spikes in the plot.

---
+++ 2D/Plot.R
@@ -0,0 +1,38 @@
+library(plotly)
+
+source("2D/WhitneyField.R")
+source("2D/WhitneySquare.R")
+source("2D/Support.R")
+
+x <- c(0.2,0.2,0.4,0.3)
+y <- c(0.3,0.1,0.5,0.9)
+coef <- matrix(c(
+    1,0,0,
+    2,0,0,
+    3,0,0,
+    4,0,0
+), byrow = TRUE, ncol = 3)
+
+field <- whitneyField(x, y, coef)
+
+W <- partition.whitney(field)
+
+# Plotting
+x_res <- 50
+y_res <- 50
+
+value <- matrix(nrow = y_res, ncol = x_res)
+
+for (i in seq_len(x_res)) {
+    for (j in seq_len(y_res)) {
+        checkpoint <- c(1.2 * (i-1) / (x_res-1) - 0.1, 1.2 * (j-1) / (y_res-1) - 0.1)
+
+        support <- searchSupport.whitney(W, checkpoint[1], checkpoint[2])
+        value[j, i] <- patch.whitney(support, checkpoint[1], checkpoint[2])
+    }
+}
+
+fig <- plot_ly(z = value)
+fig <- fig %>% add_surface()
+
+fig
