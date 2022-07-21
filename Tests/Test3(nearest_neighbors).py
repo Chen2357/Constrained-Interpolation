dea78ebd7946c9eb14aece5f475e1449151ab9e9
Chen2357 <87241041+Chen2357@users.noreply.github.com>
2022-07-21 10:59:33 -0700

    Extract test point index

--- Tests/Test3(nearest_neighbors).py
+++ Tests/Test3(nearest_neighbors).py
@@ -29,11 +29,12 @@ root.compress()
 seperation_factor = 0.5
 ws_pairs = root.well_separated_pairs_decomposition(seperation_factor)
 
-nearest_neighbors = wit.all_nearest_neighbors(ws_pairs, 2)[root.points[0].tobytes()]
+test_point = 0
+
+nearest_neighbors = wit.all_nearest_neighbors(ws_pairs, 2)[root.points[test_point].tobytes()]
 print(root.points[0])
 print(nearest_neighbors)
 print()
 
-nearest_index = np.argmin(np.max(np.abs(root.points[1:] - root.points[0]),1))
+nearest_index = np.argmin(np.max(np.abs(np.delete(root.points, test_point, 0) - root.points[test_point]),1))
 print(root.points[nearest_index + 1])
-
