dbe211625c8f949c5746f694dbbf3218b05f918c
Maxi1800 <92944189+Maxi1800@users.noreply.github.com>
2022-07-21 15:49:40 -1000

    Added documentation for Hypercube.py

--- whitney/Hypercube.py
+++ whitney/Hypercube.py
@@ -8,7 +8,25 @@ from matplotlib.patches import Rectangle
 from collections import defaultdict
 
 class Hypercube:
+    """This is a hypercube object that serves as a node in our quadtree.
+    
+    Attributes:
+        pos:        Position of the corner with smallest value in every direction.
+        width:      Side length of hypercube.
+        points:     Numpy array containing points in this Hypercube. Axis 0 used to index points, axis 1 contains their coordinates.
+        children:   List containing all children hypercubes. See [pdf] for details.
+        parent:     Parent of this cube in quadtree.
+        level:      How many subdivides to generate this hypercube from quadtree root (root hypercube has level 0).
+    """
     def __init__(self, pos: npt.ArrayLike, width: float, points: Union[npt.ArrayLike, None] = None, level: int = 0) -> None:
+        """Initialize hypercube.
+        
+        Inputs:
+            pos:    Array, length determines dimension of hypercube.
+            width:  Float.
+            points: 2D array, axis 1 must match dimesion. Default to None.
+            level:  Int Default to 0.
+        """
         self.pos = np.array(pos)
         self.width = width
         self.points = np.array(points) if points is not None else np.empty([0, len(self.pos)])
@@ -24,24 +42,29 @@ class Hypercube:
 
     @property
     def dimension(self):
+        """Dimension of hypercube, determined by length of pos attribute."""
         return len(self.pos)
 
     @property
     def isLeaf(self):
+        """True when number points in this hypercube is <= 1, False otherwise."""
         return len(self.points) <= 1
 
     @property
     def rep(self):
+        """"Representative of hypercube. It is the first element in points attribute and None when points are empty."""
         return self.points[0] if len(self.points) else None
     
     @property
     def root(self):
+        """Furthest ancestor of hypercube in quadtree."""
         cube = self
         while cube.parent is not None:
             cube = cube.parent
         return cube
 
     def subdivide(self):
+        """Populates children attribute with 2^dimension equal sized hypercubes. Partitions points into appropriate children. See [pdf] for details."""
         if self.dimension > 8:
             raise RuntimeError("Dimension of Hypercube cannot be greater than 8")
 
@@ -62,6 +85,7 @@ class Hypercube:
             self.children[i].parent = self
             
     def quadDecompose(self):
+        """Construct the quadtree recursively using subdivide such that every point is contained within a leaf node."""
         q = queue.Queue()
         q.put(self)
         while q.qsize() != 0:
@@ -73,6 +97,7 @@ class Hypercube:
                 q.put(child)
 
     def compress(self):
+        """Eliminates all internal nodes which contain only one nonempty child. Modifies parent and children attributes accordingly."""
         q = queue.Queue()
         q.put(self)
 
@@ -127,31 +152,38 @@ class Hypercube:
         ax.add_collection(pc)
 
     def well_separated_pairs_decomposition(self, s: float):
+        """Uses quadtree wih .self as root to return list of tuples of well seperated hypercubes. See [pdf] for details."""
         return well_separated_pairs(self, self, s)
     
     def contains(self, point: npt.ArrayLike):
+        """Returns True if input point lies within .self, returns False otherwise."""
         return np.all(self.pos < point) and np.all(point < self.pos + self.width)
     
     def search(self, point: npt.ArrayLike):
+        """Returns the leaf node that contaisn input point."""
+        if self.isLeaf and self.contains(point):
+            return self
         for child in self.children:
-            if child is None: continue
             if child.contains(point):
                 return child.search(point)
-        return self
+        raise ValueError("The point is not in this hypercube.")
+        
 
     def is_well_separated(self, other: "Hypercube", s: float):
+        """Returns True if .self and input other are s-well-seperated, returns False otherwise."""
         if self.isLeaf and other.isLeaf: return True
-        distance = np.max(np.abs(self.rep - other.rep))
+        distance = metric_distance(self.rep, other.rep)
         radius = self.width + other.width
 
         return radius < s * distance
 
 def well_separated_pairs(u: Hypercube, v: Hypercube, s: float):
+    """Returns well-seperated pairs for the Cartesian Product of points in u and v as a list of tuples of hypercubes."""
     if u.isLeaf and v.isLeaf and u == v: return []
     if u.rep is None or v.rep is None:
         return []
     elif u.is_well_separated(v, s):
-        return [[u, v]]
+        return [(u, v)]
     else:
         if not u.isLeaf and not v.isLeaf:
             swap = u.width < v.width # u.level > v.level
@@ -167,6 +199,7 @@ def well_separated_pairs(u: Hypercube, v: Hypercube, s: float):
         return [pair for child in children for pair in well_separated_pairs(child, v, s)]
 
 def all_nearest_neighbors(well_separated_pairs: list[list[Hypercube]], k: int):
+    """Returns dictionary containing nearest k neighbors for all points."""
     neighbors: Dict["bytes", list[npt.NDArray]] = defaultdict(list[npt.NDArray])
     for pair in well_separated_pairs:
         if len(pair[0].points) <= k:
@@ -191,45 +224,6 @@ def all_nearest_neighbors(well_separated_pairs: list[list[Hypercube]], k: int):
 
     return nearest_neighbors
 
-def filter_pairs(pairs: list[list[Hypercube]], k):
-    result = []
-    for pair in pairs:
-        if len(pair[0].points) <= k or len(pair[1].points) <= k:
-            result.append(pair)
-    return result
-
-def find_nearest_neighbor(filtered_pairs: list[list[Hypercube]], point, k):
-    neighbors = []
-    for pair in filtered_pairs:
-        if pair[0].contains(point) and len(pair[1].points) <= k:
-            for p in pair[1].points:
-                neighbors.append(p)
-        elif pair[1].contains(point) and len(pair[0].points) <= k:
-            for p in pair[0].points:
-                neighbors.append(p)
-                
-    if len(neighbors) < k:
-        raise ValueError("not enough points")
-
-    nearest_points = []
-    distances = []
-    
-    for p in neighbors:
-        if any(np.all(p == p2) for p2 in nearest_points):
-            continue            
-        
-        distance = np.max(np.abs(p - point))
-        index = np.searchsorted(distances, distance)
-        if len(nearest_points) < k:
-            distances.insert(index, distance)
-            nearest_points.insert(index, p)
-        elif index < k:
-            distances.insert(index, distance)
-            nearest_points.insert(index, p)
-            distances.pop()
-            nearest_points.pop()
-    
-    return np.array(nearest_points), np.array(distances)
-
 def metric_distance(point: npt.NDArray, points: npt.NDArray) -> npt.NDArray:
+    """Computed distance using L_infinity metric."""
     return np.max(np.abs(points - point), 1)
