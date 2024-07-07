8b8e7ac3920ea6d6384585b33448285b0264cc77
Chen Liang <87241041+Chen2357@users.noreply.github.com>
2024-07-07 14:44:15 +0800

    Add preperation time

--- Tests/CZ Runtime.py
+++ Tests/CZ Runtime.py
@@ -6,10 +6,11 @@ import timeit
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
-r = np.arange(60, 300, 40)
+r = np.arange(60, 1000, 40)
 time = []
 num_pairs = []
 
+preperation_time = []
 group_time = []
 temp_time = []
 bar_time = []
@@ -26,11 +27,15 @@ for i in r:
 
     decomposition = wit.CZ_Decomposition(root, N=2, post_shrinking=1)
 
+    # Preperation time
+    mark = timeit.default_timer()
     sigma = np.array([decomposition._sigma_0(x) for x in decomposition.points])
     groups, well_separated_pairs_indices = wit.build_wspd(decomposition.points, decomposition.s)
 
     decomposition._groups = groups
+    decomposition._diameters = decomposition._compute_diameters()
     decomposition._well_separated_pairs_indices = well_separated_pairs_indices
+    preperation_time.append(timeit.default_timer() - mark)
 
     # Group sigma
     mark = timeit.default_timer()
@@ -62,15 +67,16 @@ for i in r:
     print(i, time_diff)
 
 # %% Plot
-fig = make_subplots(rows=6, cols=1, subplot_titles=("Group sigma", "Sigma temp", "Sigma bar", "Sigma prime", "New sigma", "Total time"))
+fig = make_subplots(rows=7, cols=1, subplot_titles=("Preparation time", "Group sigma", "Sigma temp", "Sigma bar", "Sigma prime", "New sigma", "Total time"))
 
-fig.add_trace(go.Scatter(x=r, y=group_time/(r*np.log(r)), mode='lines+markers', name="Group sigma"), row=1, col=1)
-fig.add_trace(go.Scatter(x=r, y=temp_time/(r*np.log(r)), mode='lines+markers', name="Sigma temp"), row=2, col=1)
-fig.add_trace(go.Scatter(x=r, y=bar_time/(r*np.log(r)), mode='lines+markers', name="Sigma bar"), row=3, col=1)
-fig.add_trace(go.Scatter(x=r, y=prime_time/(r*np.log(r)), mode='lines+markers', name="Sigma prime"), row=4, col=1)
-fig.add_trace(go.Scatter(x=r, y=new_time/(r*np.log(r)), mode='lines+markers', name="New sigma"), row=5, col=1)
-fig.add_trace(go.Scatter(x=r, y=time/(r*np.log(r)), mode='lines+markers', name="Total time"), row=6, col=1)
+fig.add_trace(go.Scatter(x=r, y=preperation_time/(r*np.log(r)), mode='lines+markers', name="Preparation time"), row=1, col=1)
+fig.add_trace(go.Scatter(x=r, y=group_time/(r*np.log(r)), mode='lines+markers', name="Group sigma"), row=2, col=1)
+fig.add_trace(go.Scatter(x=r, y=temp_time/(r*np.log(r)), mode='lines+markers', name="Sigma temp"), row=3, col=1)
+fig.add_trace(go.Scatter(x=r, y=bar_time/(r*np.log(r)), mode='lines+markers', name="Sigma bar"), row=4, col=1)
+fig.add_trace(go.Scatter(x=r, y=prime_time/(r*np.log(r)), mode='lines+markers', name="Sigma prime"), row=5, col=1)
+fig.add_trace(go.Scatter(x=r, y=new_time/(r*np.log(r)), mode='lines+markers', name="New sigma"), row=6, col=1)
+fig.add_trace(go.Scatter(x=r, y=time/(r*np.log(r)), mode='lines+markers', name="Total time"), row=7, col=1)
 
-fig.update_layout(height=800)
+fig.update_layout(height=1000)
 fig.show()
 # %%
