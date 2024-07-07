3dae897e5091192f889d526745a53899b9da2bb9
Chen Liang <87241041+Chen2357@users.noreply.github.com>
2024-07-07 16:18:45 +0800

    Focus on the WSPD issue

--- Tests/CZ Runtime.py
+++ Tests/CZ Runtime.py
@@ -3,6 +3,8 @@ from test_module import *
 from actual_CZ import *
 import timeit
 
+import wspd
+
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
@@ -10,7 +12,8 @@ r = np.arange(60, 1000, 40)
 time = []
 num_pairs = []
 
-preperation_time = []
+wspd_time = []
+experiment_time = []
 group_time = []
 temp_time = []
 bar_time = []
@@ -27,51 +30,76 @@ for i in r:
 
     decomposition = wit.CZ_Decomposition(root, N=2, post_shrinking=1)
 
-    # Preperation time
+    # WSPD time
     mark = timeit.default_timer()
-    decomposition._pre_arpproximate_sigma()
-    preperation_time.append(timeit.default_timer() - mark)
+    wit.build_wspd(E, 2)
+    wspd_time.append(timeit.default_timer() - mark)
+
+    # Experiment time
+    mark = timeit.default_timer()
+    num_points = len(E)
+    dim = len(E[0])
+    wspd_points = [wspd.point(E[i]) for i in range(num_points)]  # type: ignore
+
+    L_half = wspd.build_wspd(num_points, dim, 2, wspd_points) # type: ignore
+    L_half: list[tuple[list[int], list[int]]]
+
+    L = L_half + [(B, A) for A, B in L_half]
+
+    T_with_duplicates = [A for tup in L for A in tup]
+
+    # Adding the following makes the time complexity not O(r log(r))
+    # T = []
+    # for A in T_with_duplicates:
+    #     if A not in T:
+    #         T.append(A)
+    experiment_time.append(timeit.default_timer() - mark)
 
     # Group sigma
-    mark = timeit.default_timer()
-    decomposition._compute_group_sigma()
-    group_time.append(timeit.default_timer() - mark)
+    # mark = timeit.default_timer()
+    # decomposition._compute_group_sigma()
+    # group_time.append(timeit.default_timer() - mark)
 
-    # Sigma temp
-    mark = timeit.default_timer()
-    decomposition._compute_sigma_temp()
-    temp_time.append(timeit.default_timer() - mark)
+    # # Sigma temp
+    # mark = timeit.default_timer()
+    # decomposition._compute_sigma_temp()
+    # temp_time.append(timeit.default_timer() - mark)
 
-    # Sigma bar
-    mark = timeit.default_timer()
-    decomposition._compute_sigma_bar()
-    bar_time.append(timeit.default_timer() - mark)
+    # # Sigma bar
+    # mark = timeit.default_timer()
+    # decomposition._compute_sigma_bar()
+    # bar_time.append(timeit.default_timer() - mark)
 
-    # Sigma prime
-    mark = timeit.default_timer()
-    decomposition._compute_sigma_prime()
-    prime_time.append(timeit.default_timer() - mark)
+    # # Sigma prime
+    # mark = timeit.default_timer()
+    # decomposition._compute_sigma_prime()
+    # prime_time.append(timeit.default_timer() - mark)
 
-    # New sigma
-    mark = timeit.default_timer()
-    decomposition._compute_new_sigma()
-    new_time.append(timeit.default_timer() - mark)
+    # # New sigma
+    # mark = timeit.default_timer()
+    # decomposition._compute_new_sigma()
+    # new_time.append(timeit.default_timer() - mark)
 
     time_diff = timeit.default_timer() - start_time
     time.append(time_diff)
     print(i, time_diff)
 
 # %% Plot
-fig = make_subplots(rows=7, cols=1, subplot_titles=("Preparation time", "Group sigma", "Sigma temp", "Sigma bar", "Sigma prime", "New sigma", "Total time"))
+def plot(titles, times):
+    rows = len(titles)
+    fig = make_subplots(rows=rows, cols=1, subplot_titles=titles)
 
-fig.add_trace(go.Scatter(x=r, y=preperation_time/(r*np.log(r)), mode='lines+markers', name="Preparation time"), row=1, col=1)
-fig.add_trace(go.Scatter(x=r, y=group_time/(r*np.log(r)), mode='lines+markers', name="Group sigma"), row=2, col=1)
-fig.add_trace(go.Scatter(x=r, y=temp_time/(r*np.log(r)), mode='lines+markers', name="Sigma temp"), row=3, col=1)
-fig.add_trace(go.Scatter(x=r, y=bar_time/(r*np.log(r)), mode='lines+markers', name="Sigma bar"), row=4, col=1)
-fig.add_trace(go.Scatter(x=r, y=prime_time/(r*np.log(r)), mode='lines+markers', name="Sigma prime"), row=5, col=1)
-fig.add_trace(go.Scatter(x=r, y=new_time/(r*np.log(r)), mode='lines+markers', name="New sigma"), row=6, col=1)
-fig.add_trace(go.Scatter(x=r, y=time/(r*np.log(r)), mode='lines+markers', name="Total time"), row=7, col=1)
+    for i in range(rows):
+        fig.add_trace(go.Scatter(x=r, y=times[i]/(r*np.log(r)), mode='lines+markers', name=titles[i]), row=i+1, col=1)
 
-fig.update_layout(height=1000)
-fig.show()
+    fig.update_layout(height=200*rows)
+    fig.show()
+
+plot([
+    "WSPD time",
+    "Experiment time"
+    ], [
+    wspd_time,
+    experiment_time
+])
 # %%
